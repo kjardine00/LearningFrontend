7f4550ad8f9e300b973b489b0445b1aff19b85ce
Kyle <kjardine00@gmail.com>
2025-07-10 21:28:35 -0400

    progress is being made

--- React-CourseFollowAlong/src/App.jsx
+++ React-CourseFollowAlong/src/App.jsx
@@ -1,66 +1,18 @@
 import "./App.css";
-import TodoTitle from "./components/TodoTitle.jsx";
-import Todo from "./components/Todo.jsx";
-import Popup from "./components/Popup.jsx";
-import Counter from "./components/Counter.jsx";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
-  const [popupOpen, setPopupOpen] = useState(false);
-
-  function togglePopup() {
-    console.log("parent delete notified");
-    setPopupOpen((prevPopupOpen) => !prevPopupOpen);
-  }
-
-  function closePopup() {
-    setPopupOpen(false);
-  }
-
-  useEffect(() => {
-    console.log("component mounted");
-  }, []);
-
   return (
-    <>
-      <TodoTitle />
-      <div>
-        <input
-          type="text"
-          placeholder="Task"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setPopupOpen(true)}>Add Task</button>
-      </div>
-      <Todo
-        togglePopup={togglePopup}
-        task="Learn React"
-        desc="Follow the tutorial on youtube"
-      />
-      <Todo
-        togglePopup={togglePopup}
-        task="Learn Next.js"
-        desc="Follow the tutorial on youtube"
-      />
-      <Todo
-        togglePopup={togglePopup}
-        task="Get the Job"
-        desc="apply to jobs everyday"
-      />
-      <Todo
-        togglePopup={togglePopup}
-        task="Make money"
-        desc="relax and enjoy life"
-      />
-      {popupOpen ? (
-        <Popup
-          closePopup={closePopup}
-          prompt="Are you really really sure????"
-        />
-      ) : null}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
